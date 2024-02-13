e44229e4963288a8d9cd245e6482dbf45e638b6e
Patricio Mena <luispatricio.mh@gmail.com>
2024-02-13 15:34:42 -0600

    added container and section to base style

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 
 export default function App() {
   return (
-    <>
+    <div className="section">
       <h1>My Travel Journey 2</h1>
       <p>My Travel Journey</p>
-    </>
+    </div>
   );
 }
