d4831cc894157a6db9f10aa57315e29f1d9fae2b
Patricio Mena <luispatricio.mh@gmail.com>
2024-02-14 11:57:53 -0600

    finished react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 // import viteLogo from '/vite.svg';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import data from './data';
 import './App.css';
 
 export default function App() {
+  const travelCards = data.map((card) => {
+    return <Card key={card.id} {...card} />;
+  });
   return (
     <>
       <Navbar />
       <section className="section">
-        <div className="container">
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        <div className="container">{travelCards}</div>
       </section>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
-export default function Card() {
+export default function Card(props) {
   return (
     <article className="card">
-      <img class="card--img" src="https://source.unsplash.com/WLxQvbMyfas" alt="Mount Fuji" />
+      <img className="card--img" src={props.imageUrl} alt={props.title} />
       <div className="card--content">
         <div>
           <div className="card--content_heading">
@@ -13,26 +13,24 @@ export default function Card() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M3.57866 5.25002C2.65532 5.25002 1.90837 4.50221 1.90837 3.57965C1.90837 2.65757 2.65532 1.90911 3.57866 1.90911C4.50167 1.90911 5.24927 2.65757 5.24927 3.57965C5.24927 4.50221 4.50167 5.25002 3.57866 5.25002M3.48394 0C1.54227 0 0 1.63678 0 3.65569C0 6.40791 2.95078 9.25191 2.95078 9.25191C3.34777 9.62783 3.59437 9.65847 4.01646 9.25191C4.01646 9.25191 7 6.40791 7 3.65569C7 1.63678 5.42544 0 3.48394 0"
                   fill="#F55A5A"
                 />
               </svg>
-              <span>JAPAN</span>
+              <span>{props.location.toUpperCase()}</span>
               <a href="">View On Google Maps</a>
             </div>
-            <h2 className="mt-sm">Mount Fuji</h2>
+            <h2 className="mt-sm">{props.title}</h2>
           </div>
         </div>
 
         <div>
-          <time className="bold">12 Jan, 2021 - 24 Jan, 2021</time>
-          <p className="mt-sm">
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet).
-            Mount Fuji is the single most popular tourist site in Japan, for both Japanese and
-            foreign tourists.
-          </p>
+          <time className="bold">
+            {props.startDate} - {props.endDate}
+          </time>
+          <p className="mt-sm">{props.description}</p>
         </div>
       </div>
     </article>
